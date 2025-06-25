00f95525a62f7b0e906193668c26c9b77ff71835
DelRiscoAriel <69070833+DelRiscoAriel@users.noreply.github.com>
2025-06-25 03:14:18 -0400

    Update settings.py

--- freelancer/settings.py
+++ freelancer/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'freelancer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'tracker/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
